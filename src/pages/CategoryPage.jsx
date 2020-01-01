2c80acb33b38f49443749760bdd7fdc5beed2bbf
pandemuliada <pandemuliada@gmail.com>
2020-01-01 20:41:56 +0800

    :sparkles: create edit feature on category

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
 import { db } from '../firebase'
 import Table from '../components/Table'
 import { TextField } from '../components/inputs'
@@ -12,6 +13,11 @@ const CategoryPage = () => {
   const [isLoading, setIsLoading] = useState(false)
 
   const [isAdd, setIsAdd] = useState(false)
+  const [isEdit, setIsEdit] = useState(false)
+  const [isDelete, setIsDelete] = useState(false)
+
+  const [editedItem, setEditedItem] = useState(null)
+  const [deletedItem, setDeletedItem] = useState(null)
 
   const columns = [
     { 
@@ -22,14 +28,28 @@ const CategoryPage = () => {
     { 
       key: 'created_at', 
       label: 'Created at',
-      render: ({ created_at }) => ('render', created_at.seconds)
+      render: ({ created_at }) => dayjs(created_at).format('dddd, MMMM D YYYY')
     },
     { 
       key: 'options', 
       label: 'Options',
       render: (item) => (<div className='flex items-center'>
-        <button className='p-1 text-gray-400 hover:text-yellow-400' onClick={() => console.log(item)}><IoIosCreate size={22}/></button>
-        <button className='p-1 text-gray-400 hover:text-red-400' onClick={() => console.log(item)}><IoIosTrash size={22}/></button>
+        <button 
+          className='p-1 text-gray-400 hover:text-yellow-400' 
+          onClick={() => { 
+            setIsEdit(true) 
+            setEditedItem(item)
+          }}>
+          <IoIosCreate size={22}/>
+        </button>
+        <button 
+          className='p-1 text-gray-400 hover:text-red-400' 
+          onClick={() => {
+            setIsDelete(true)
+            setDeletedItem(item)
+          }}>
+          <IoIosTrash size={22}/>
+        </button>
       </div>)
     },
   ]
@@ -62,7 +82,7 @@ const CategoryPage = () => {
 
   async function onCommitAdd(values) {
     setIsLoading(true)
-    const saved = await db.collection("categories").add({
+    const saved = db.collection("categories").add({
       ...values,
       created_at: Date.now()
     })
@@ -74,12 +94,34 @@ const CategoryPage = () => {
     }
   }
 
+  async function onCommitEdit(values) {
+    setIsLoading(true)
+    const edited = db.collection("categories").doc(editedItem.id).update({
+      ...values,
+      updated_at: Date.now()
+    })
+
+    if (edited) {
+      setIsEdit(false)
+      setEditedItem(null)
+      setIsLoading(false)
+      onLoadPage()
+    }
+  }
+
 
   return (<div>
     <Panel title='Category' size='small' isOpen={isAdd} onClose={() => setIsAdd(false)}>
       <CategoryForm
         onSubmit={onCommitAdd}
         onCancel={() => setIsAdd(false)} />
+    </Panel>
+    
+    <Panel title='Category' size='small' isOpen={isEdit} onClose={() => setIsEdit(false)}>
+      <CategoryForm
+        initialValues={editedItem}
+        onSubmit={onCommitEdit}
+        onCancel={() => setIsEdit(false)} />
     </Panel>
     <div className='flex items-center bg-white py-4 px-6 shadow mb-6 rounded'>
       <h1 className='text-2xl font-medium text-gray-600'>Category</h1>
