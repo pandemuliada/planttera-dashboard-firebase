4e160b78b544b70d8d649114ad4f70446e6dc9d0
pandemuliada <pandemuliada@gmail.com>
2020-04-12 21:51:10 +0800

    :recycle: user account page - update data

--- src/components/forms/UserAccountForm.jsx
+++ src/components/forms/UserAccountForm.jsx
@@ -3,53 +3,72 @@ import { Formik } from 'formik'
 import { string, object } from 'yup'
 import { FormikTextField } from '../inputs'
 import { Button, OutlineButton } from '../buttons'
+import { formatError } from '../../utils/format'
 
 const defaultValues = {
-  displayName: '',
+  name: '',
   email: '',
 }
 
 const userAccountFormSchema = object().shape({
-  displayName: string().required('Cannot be empty'),
+  name: string().required('Cannot be empty'),
   email: string().required('Cannot be empty'),
 })
 
-const UserAccountForm = (props) => {
-  const { 
-    initialValues,
-    onSubmit,
-    onCancel,
-  } = props
+const UserAccountForm = props => {
+  const { initialValues, onSubmit, onCancel } = props
 
   function onSubmitForm(values, callback) {
-    onSubmit(values).then(() => {
-      callback.resetForm()
-    }).catch(error => {
-      console.log(error.response)
-    })
+    const { resetForm, setSubmitting, setErrors } = callback
+    onSubmit(values)
+      .then(() => {
+        resetForm()
+      })
+      .catch(error => {
+        setErrors(formatError(error.response.data.errors))
+        setSubmitting(false)
+      })
   }
-  
-  return (<Formik enableReinitialize initialValues={initialValues ? initialValues : defaultValues} validationSchema={userAccountFormSchema} onSubmit={onSubmitForm}>
-    {({ handleSubmit, isSubmitting, isValid, handleReset }) => {
-      return(<form onSubmit={handleSubmit}>
-        <FormikTextField size='small' name='displayName' label='Name*' />
-        <FormikTextField size='small' name='email' label='Email*' />
 
-        <div className='flex justify-end'>
-          <div className='mr-2'>
-            <Button size='small' type='submit' disabled={!isValid} loading={isSubmitting}>Submit</Button>
-          </div>
-          <div className='mr-2'>
-            <OutlineButton color='secondary' size='small' type='submit' disabled={isSubmitting} onClick={() => {
-              onCancel()
-              handleReset()
-            }}>Cancel</OutlineButton>
-          </div>
-        </div>
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={initialValues ? initialValues : defaultValues}
+      validationSchema={userAccountFormSchema}
+      onSubmit={onSubmitForm}
+    >
+      {({ handleSubmit, isSubmitting, isValid, handleReset }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <FormikTextField size="small" name="name" label="Name*" />
+            <FormikTextField size="small" name="email" label="Email*" />
 
-      </form>)
-    }}
-  </Formik>)
+            <div className="flex justify-end">
+              <div className="mr-2">
+                <Button size="small" type="submit" disabled={!isValid} loading={isSubmitting}>
+                  Submit
+                </Button>
+              </div>
+              <div className="mr-2">
+                <OutlineButton
+                  color="secondary"
+                  size="small"
+                  type="submit"
+                  disabled={isSubmitting}
+                  onClick={() => {
+                    onCancel()
+                    handleReset()
+                  }}
+                >
+                  Cancel
+                </OutlineButton>
+              </div>
+            </div>
+          </form>
+        )
+      }}
+    </Formik>
+  )
 }
 
 export default UserAccountForm
--- src/pages/UserAccountPage.jsx
+++ src/pages/UserAccountPage.jsx
@@ -3,6 +3,7 @@ import dayjs from 'dayjs'
 import { useState } from 'react'
 import { auth, storage } from '../firebase'
 import { CurrentUserContext } from '../contexts/CurrentUserContext'
+
 import { IconButton } from '../components/buttons'
 import Tabs from '../components/Tabs'
 import Panel from '../components/Panel'
@@ -12,49 +13,33 @@ import PasswordForm from '../components/forms/PasswordForm'
 import PictureForm from '../components/forms/PictureForm'
 
 import defaultImage from '../static/images/no-image.png'
+import { privateApi } from '../utils/request'
 
 const UserProfilePage = () => {
-  const { currentUser, setCurrentUser } = useContext(CurrentUserContext)
+  const { currentUser, getCurrentUser } = useContext(CurrentUserContext)
   const [isEdit, setIsEdit] = useState(false)
   const [activeTab, setActiveTab] = useState('profile')
 
-  const defaultToastState = { 
-    isShow: false, 
+  const defaultToastState = {
+    isShow: false,
     type: 'default',
-    duration: 4000, // remove duration property to prevent autoclose after 4s 
+    duration: 4000, // remove duration property to prevent autoclose after 4s
   }
   const [toast, setToast] = useState(defaultToastState)
 
   async function onCommitEditData(values) {
-    try {
-      const updatedProfile = await auth.currentUser.updateProfile({
-        displayName: values.displayName,
-      })
+    const response = await privateApi().put('users/current_user/update', { ...values })
 
-      const updatedEmail = await auth.currentUser.updateEmail(values.email)
-      
-      if (updatedProfile === undefined && updatedEmail === undefined) {
-        setCurrentUser({...currentUser, ...values})
-        setIsEdit(false)
-        setToast({
-          ...toast, 
-          isShow: true,
-          type: 'primary',
-          title: 'Data Updated',
-          message: 'Your data has been updated' 
-        })
-      }
-    } catch (error) {
-      if (error.code === 'auth/requires-recent-login') {
-        setIsEdit(false)
-        setToast({
-          ...toast, 
-          isShow: true,
-          type: 'danger',
-          title: 'Sensitive Operation',
-          message: 'Login again before retrying change your email'
-        })
-      }
+    if (response && response.data.data) {
+      setIsEdit(false)
+      getCurrentUser()
+      setToast({
+        ...toast,
+        isShow: true,
+        type: 'primary',
+        title: 'User Updated',
+        message: 'Your data has been updated!',
+      })
     }
   }
 
@@ -64,121 +49,129 @@ const UserProfilePage = () => {
       if (updatedPassword === undefined) {
         setIsEdit(false)
         setToast({
-          ...toast, 
+          ...toast,
           isShow: true,
           type: 'primary',
           title: 'Password Updated',
-          message: 'Password updated successfully' 
+          message: 'Password updated successfully',
         })
       }
     } catch (error) {
       if (error.code === 'auth/requires-recent-login') {
         setIsEdit(false)
         setToast({
-          ...toast, 
+          ...toast,
           isShow: true,
           type: 'danger',
           title: 'Sensitive Operation',
-          message: 'Login again before retrying change your password' 
+          message: 'Login again before retrying change your password',
         })
       }
-    } 
-  }
-
-  async function onCommitChangePicture(file) {
-    const fileExtension = file.type.split('/')[1]
-
-    const storageRef = storage.ref('profiles/' + 'user_' + currentUser.uid + '.' + fileExtension)
-    const snapshot = await storageRef.put(file)
-    const url = await snapshot.ref.getDownloadURL()
-
-    const updatedProfile = await auth.currentUser.updateProfile({
-      photoURL:  url,
-    })
-    
-    if (snapshot.state === 'success' && updatedProfile === undefined) {
-      setIsEdit(false)
-      setToast({
-        ...toast, 
-        isShow: true,
-        type: 'primary',
-        title: 'Picture Updated',
-        message: 'Profile picture changed successfully' 
-      })
     }
   }
 
-  return (<div>
-    <Toaster
-      isShow={toast.isShow}
-      type={toast.type}
-      duration={toast.duration}
-      title={toast.title}
-      message={toast.message}
-      onClose={() => setToast({...toast, ...defaultToastState})}/>
+  async function onCommitChangePicture(file) {
+    // const fileExtension = file.type.split('/')[1]
+    // const storageRef = storage.ref('profiles/' + 'user_' + currentUser.uid + '.' + fileExtension)
+    // const snapshot = await storageRef.put(file)
+    // const url = await snapshot.ref.getDownloadURL()
+    // const updatedProfile = await auth.currentUser.updateProfile({
+    //   photoURL: url,
+    // })
+    // if (snapshot.state === 'success' && updatedProfile === undefined) {
+    //   setIsEdit(false)
+    //   setToast({
+    //     ...toast,
+    //     isShow: true,
+    //     type: 'primary',
+    //     title: 'Picture Updated',
+    //     message: 'Profile picture changed successfully',
+    //   })
+    // }
+  }
 
-    <Panel title='User Account' size='small' isOpen={isEdit} onClose={() => setIsEdit(false)}>
-      <div className='mb-5'>
-        <Tabs 
-          items={[
-            { key: 'profile', label: "Profile" },
-            { key: 'password', label: "Password" },
-            { key: 'picture', label: "Picture" },
-          ]}
-          activeTab={activeTab}
-          onChangeTab={(key) => setActiveTab(key)}
-        />
-      </div>
-      {activeTab === 'profile' && 
-        <UserAccountForm
-          initialValues={currentUser}
-          onSubmit={onCommitEditData} 
-          onCancel={() => setIsEdit(false)} />}
-      {activeTab === 'password' && 
-        <PasswordForm 
-          onSubmit={onCommitChangePassword} 
-          onCancel={() => setIsEdit(false)} />}
-      {activeTab === 'picture' && 
-        <PictureForm 
-          initialImage={(!!currentUser && currentUser.photoURL) || defaultImage}
-          onSubmit={(file) => onCommitChangePicture(file)}
-          onCancel={() => setIsEdit(false)} />}
-    </Panel>
+  return (
+    <div>
+      <Toaster
+        isShow={toast.isShow}
+        type={toast.type}
+        duration={toast.duration}
+        title={toast.title}
+        message={toast.message}
+        onClose={() => setToast({ ...toast, ...defaultToastState })}
+      />
 
-    <div className='flex items-center bg-white py-4 px-6 shadow mb-6 rounded'>
-      <h1 className='text-2xl font-medium text-gray-600'>Account Detail</h1>
-      <span className='ml-auto text-gray-600'>{dayjs().format("dddd, MMMM D YYYY")}</span>
-    </div>
-  
-    <div className='flex items-start'>
-      <div className='bg-white w-1/3 mr-5 py-4 px-6 shadow mb-8 rounded'>
-        <img className='mx-auto' src={(!!currentUser && currentUser.photoURL) || defaultImage} alt={!!currentUser.displayName ? currentUser.displayName : ''}/>
+      <Panel title="User Account" size="small" isOpen={isEdit} onClose={() => setIsEdit(false)}>
+        <div className="mb-5">
+          <Tabs
+            items={[
+              { key: 'profile', label: 'Profile' },
+              { key: 'password', label: 'Password' },
+              { key: 'picture', label: 'Picture' },
+            ]}
+            activeTab={activeTab}
+            onChangeTab={key => setActiveTab(key)}
+          />
+        </div>
+        {activeTab === 'profile' && (
+          <UserAccountForm
+            initialValues={currentUser}
+            onSubmit={onCommitEditData}
+            onCancel={() => setIsEdit(false)}
+          />
+        )}
+        {activeTab === 'password' && (
+          <PasswordForm onSubmit={onCommitChangePassword} onCancel={() => setIsEdit(false)} />
+        )}
+        {activeTab === 'picture' && (
+          <PictureForm
+            initialImage={(!!currentUser && currentUser.picture) || defaultImage}
+            onSubmit={file => onCommitChangePicture(file)}
+            onCancel={() => setIsEdit(false)}
+          />
+        )}
+      </Panel>
+
+      <div className="flex items-center bg-white py-4 px-6 shadow mb-6 rounded">
+        <h1 className="text-2xl font-medium text-gray-600">Account Detail</h1>
+        <span className="ml-auto text-gray-600">{dayjs().format('dddd, MMMM D YYYY')}</span>
       </div>
-      <div className='bg-white w-2/3 py-4 px-6 shadow mb-8 rounded'>
-        <table className='table-auto rounded w-full'>
-          <tbody>
-            <tr className='bg-gray-100'>
-              <td className='p-3 w-24'>Name</td>
-              <td className='p-3'>: {!!currentUser.displayName ? currentUser.displayName : '-'}</td>
-            </tr>
-            <tr className='bg-white'>
-              <td className='p-3 w-24'>Email</td>
-              <td className='p-3'>: {!!currentUser.email ? currentUser.email : '-'}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div className='flex justify-end mt-4'>
-          <IconButton
-            outline 
-            size='small' 
-            icon='pencil' 
-            onClick={() => {
-              setIsEdit(true)
-            }}/>
+
+      <div className="flex items-start">
+        <div className="bg-white w-1/3 mr-5 py-4 px-6 shadow mb-8 rounded">
+          <img
+            className="mx-auto"
+            src={(!!currentUser && currentUser.picture) || defaultImage}
+            alt={!!currentUser ? currentUser.name : ''}
+          />
+        </div>
+        <div className="bg-white w-2/3 py-4 px-6 shadow mb-8 rounded">
+          <table className="table-auto rounded w-full">
+            <tbody>
+              <tr className="bg-gray-100">
+                <td className="p-3 w-24">Name</td>
+                <td className="p-3">: {!!currentUser ? currentUser.name : '-'}</td>
+              </tr>
+              <tr className="bg-white">
+                <td className="p-3 w-24">Email</td>
+                <td className="p-3">: {!!currentUser ? currentUser.email : '-'}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="flex justify-end mt-4">
+            <IconButton
+              outline
+              size="small"
+              icon="pencil"
+              onClick={() => {
+                setIsEdit(true)
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>)
+  )
 }
 
 export default UserProfilePage
